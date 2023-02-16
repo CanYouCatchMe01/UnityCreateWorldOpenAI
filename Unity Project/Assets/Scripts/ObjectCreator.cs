4c84ee673fa782f531eb28f2037928574b0e5cc2
Marcus Cazzola <marcus.cazzola@gmail.com>
2023-02-16 21:09:55 +0100

    added NuGet extension to Unity

--- Unity Project/Assets/Scripts/ObjectCreator.cs
+++ Unity Project/Assets/Scripts/ObjectCreator.cs
@@ -3,6 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using OpenAI_API;
+using Microsoft.CognitiveServices.Speech;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 public class ObjectCreator : MonoBehaviour
 {
@@ -15,9 +19,14 @@ public class ObjectCreator : MonoBehaviour
     public MeshData[] myMeshData;
     public TMP_InputField myInputfield;
 
+    //API
+    OpenAI_API.OpenAIAPI myOpenAIAPI;
+    SpeechRecognizer mySpeechRecognizer;
+
     void Start()
     {
-        
+        string secretsPath = Application.streamingAssetsPath + "/secrets.json";
+        JObject secrets = JObject.Parse(File.ReadAllText(secretsPath));
     }
 
     void Update()
